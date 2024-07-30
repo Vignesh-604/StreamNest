ef6155f811b6995c41ea6546a9c270eca3d9d525
Vignesh-604 <vig6604@gmail.com>
2024-07-30 12:26:26 +0530

    MAde few changes to VideoItem and userlogin in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import Navbar from './components/Navbar'
 import VideoItem from './components/VideoItem'
 import LikedVideos from './components/LikedVideos'
+import Playlists from './components/Playlists'
 
 function App() {
-
+        // axios.post("/api/users/login",{
+        //     username: "bobo",
+        //     email: "bob@gmail.com",
+        //     password: "12345678"
+        //   }
+        //   )
+        // .then((res) => console.log(res))
+        // .catch(error => console.log(error))
   return (
     <>
     <Navbar />
     <div className=' min-w-3xl'>
       <LikedVideos />
+      <Playlists/>
       {/* <h1>Welcome to StreamNest</h1> */}
     </div>
     </>
--- Frontend/src/components/VideoItem.jsx
+++ Frontend/src/components/VideoItem.jsx
@@ -2,16 +2,23 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import thumbnailAlt from "../assets/thumbnail.jpeg"
 
-export default function VideoItem({id, title, description, owner, views, thumbnail, duration, remove }) {
+export default function VideoItem({ id, title, description, owner, views, thumbnail, duration, remove }) {
     return (
-        <div className="flex items-start space-x-4 my-4 w-full">
-            <div className="relative w-56 flex-shrink-0">
+        <div className="flex max-w-2xl p-3 flex-col items-start md:flex-row hover:bg-slate-800 rounded-md my-3 w-full">
+            <div className="relative w-full flex-shrink-0 mx-auto md:w-56 md:me-4">
+                {/* Cross mark */}
+                <button onClick={() => remove(id)} className="absolute left-1">
+                    <XMarkIcon className="m-2 h-7 w-7 text-white hover:bg-gray-500 hover:bg-opacity-15 rounded-xl" />
+                </button>
+                {/* Thumbnail */}
                 <img src={thumbnail} onError={e => e.target.src = thumbnailAlt} alt="thumbnail" className="w-full h-32 object-cover rounded-lg" />
+                {/* Duration  */}
                 <span className="absolute bottom-1 right-1 bg-black text-white text-xs p-1 rounded-sm">
                     {duration}
                 </span>
             </div>
-            <div className="flex flex-col justify-between flex-grow">
+            {/* Title, description and other details */}
+            <div className="flex flex-col justify-between flex-grow mt-2">
                 <div className="text-sm">
                     <h1 className="font-semibold text-xl truncate">{title}</h1>
                     <h1 className="text-gray-400">
@@ -23,9 +30,7 @@ export default function VideoItem({id, title, description, owner, views, thumbna
                     </h1>
                 </div>
             </div>
-            <button onClick={() => remove(id)}>
-                <XMarkIcon  className="ms-5 h-5 w-5 text-white" />
-            </button>
+
         </div>
     );
 }
