5d5875d77914776ff2a5709ce465e8d149a256af
Vignesh-604 <vig6604@gmail.com>
2024-08-04 21:39:21 +0530

    Added options icon in Post and comments (static)

--- Frontend/src/components/Post/Comment.jsx
+++ Frontend/src/components/Post/Comment.jsx
@@ -1,23 +1,56 @@
-import { ThumbsUp, ThumbsUpIcon } from 'lucide-react';
+import { ThumbsUp, EllipsisVertical } from 'lucide-react';
 import img from "../assets/profile.webp"
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import {
+    PencilIcon,
+    TrashIcon,
+} from '@heroicons/react/16/solid'
 
 
 export default function Comment({ id, content, updatedAt, likes, isLiked, fullname, username, avatar, toggleLike }) {
     return (
         <div className="container mx-auto p-4">
             <div className="border text-white p-4 rounded-lg mb-4 shadow-md">
-                <div className="flex items-center mb-4">
-                    <img
-                        src={avatar}
-                        alt={`${fullname} avatar`}
-                        onError={e => e.target.src = img}
-                        className="h-10 w-10 rounded-full object-cover mr-3"
-                    />
-                    <div>
-                        <span className="font-semibold">{fullname}</span>
-                        <span className="font-normal ms-2 text-gray-400">@{username}</span>
-                        <div className="text-sm text-gray-400">{updatedAt}</div>
+                <div className="flex items-center mb-4 justify-between">
+                    <div className='flex items-center'>
+                        <img
+                            src={avatar}
+                            alt={`${fullname} avatar`}
+                            onError={e => e.target.src = img}
+                            className="h-10 w-10 rounded-full object-cover mr-3"
+                        />
+                        <div>
+                            <span className="font-semibold">{fullname}</span>
+                            <span className="font-normal ms-2 text-gray-400">@{username}</span>
+                            <div className="text-sm text-gray-400">{updatedAt}</div>
+                        </div>
                     </div>
+                    <button className='flex place-items-start'>
+                        <Menu>
+                            <MenuButton className="">
+                                <EllipsisVertical />
+                            </MenuButton>
+
+                            <MenuItems
+                                transition
+                                anchor="bottom end"
+                                className="w-52 origin-top-right rounded-xl border border-white/20 bg-gray-800 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+                            >
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                        <PencilIcon className="size-4 fill-white/30" />
+                                        Edit
+                                    </button>
+                                </MenuItem>
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                        <TrashIcon className="size-4 fill-white/30" />
+                                        Delete
+                                    </button>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    </button>
                 </div>
                 <div className="mb-4 whitespace-pre-line">{content}</div>
                 <div className="flex justify-between text-gray-400">
--- Frontend/src/components/Post/Post.jsx
+++ Frontend/src/components/Post/Post.jsx
@@ -1,9 +1,14 @@
-import { ThumbsUp } from 'lucide-react';
+import { EllipsisVertical, ThumbsUp } from 'lucide-react';
 import img from "../assets/profile.webp"
 import Comment from './Comment';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { parseDate } from '../utility';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import {
+    PencilIcon,
+    TrashIcon,
+} from '@heroicons/react/16/solid'
 
 
 export default function PostItem({ postId = "66adea053ce85ffaeda677dd" }) {
@@ -12,7 +17,7 @@ export default function PostItem({ postId = "66adea053ce85ffaeda677dd" }) {
     const [comments, setComments] = useState([])
 
     useEffect(() => {
-        axios.get(`/api//post/${postId}`)
+        axios.get(`/api/post/${postId}`)
             .then((res) => setPost(res.data.data))
             .catch(error => console.log(error));
 
@@ -52,18 +57,46 @@ export default function PostItem({ postId = "66adea053ce85ffaeda677dd" }) {
             {
                 post ? (
                     <div className="bg-gray-800 text-white p-4 rounded-lg mb-4 shadow-md">
-                        <div className="flex items-center mb-4">
-                            <img
-                                src={post.owner[0].avatar}
-                                alt={`${post.name} avatar`}
-                                onError={e => e.target.src = img}
-                                className="h-10 w-10 rounded-full object-cover mr-3"
-                            />
-                            <div>
-                                <span className="font-semibold">{post.owner[0].fullname}</span>
-                                <span className="font-normal ms-2 text-gray-400">@{post.owner[0].username}</span>
-                                <div className="text-sm text-gray-400">{parseDate(post.updatedAt)}</div>
+                        <div className="flex items-center mb-4 justify-between">
+                            <div className='flex items-center'>
+                                <img
+                                    src={post.owner[0].avatar}
+                                    alt={`${post.name} avatar`}
+                                    onError={e => e.target.src = img}
+                                    className="h-10 w-10 rounded-full object-cover mr-3"
+                                />
+                                <div>
+                                    <span className="font-semibold">{post.owner[0].fullname}</span>
+                                    <span className="font-normal ms-2 text-gray-400">@{post.owner[0].username}</span>
+                                    <div className="text-sm text-gray-400">{parseDate(post.updatedAt)}</div>
+                                </div>
                             </div>
+                            <button className='flex place-items-start'>
+                                <Menu>
+                                    <MenuButton className="">
+                                        <EllipsisVertical />
+                                    </MenuButton>
+
+                                    <MenuItems
+                                        transition
+                                        anchor="bottom end"
+                                        className="w-52 origin-top-right rounded-xl border border-white/20 bg-gray-800 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+                                    >
+                                        <MenuItem>
+                                            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                                <PencilIcon className="size-4 fill-white/30" />
+                                                Edit
+                                            </button>
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                                <TrashIcon className="size-4 fill-white/30" />
+                                                Delete
+                                            </button>
+                                        </MenuItem>
+                                    </MenuItems>
+                                </Menu>
+                            </button>
                         </div>
                         <div className="mb-4 whitespace-pre-line">{post.content}</div>
                         <div className="flex justify-between text-gray-400">
