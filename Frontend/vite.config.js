4153b8c02e1389b965f5e5632b21efe4abb68f35
Vignesh-604 <vig6604@gmail.com>
2024-08-18 15:29:08 +0530

    Added Proxy URL for backend in ENV file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy : {
-      "/api" : "http://localhost:8000"
+      "/api" : process.env.VITE_PROXY
     }
   },
   plugins: [react()],
