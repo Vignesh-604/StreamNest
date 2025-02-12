d6ff0cc72c50be8605bc2760fb48d0319970833e
Vignesh-604 <vig6604@gmail.com>
2025-02-13 01:49:59 +0530

    Updated dependencies and tailwind. Removed postcss.config. Fixed db import

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import tailwindcss from '@tailwindcss/vite'
 
 dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy : {
-      "/api" : process.env.VITE_PROXY
+    proxy: {
+      "/api": process.env.VITE_PROXY
     }
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
